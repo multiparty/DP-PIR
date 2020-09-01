821f8a45cbe634d5ec4ee61c5dfb6d5b0d070427
Kinan Dak Albab <kinan.bab.14@gmail.com>
2020-08-31 20:12:31 -0400

    Clean up client-side code

--- client/protocols.js
+++ client/protocols.js
@@ -1,4 +1,8 @@
-/* global TOTAL_COUNT, getPointById */
+/* global UI */
+(function () {
+
+// Sends a query over a web socket, and returning a promise to the service's
+// response.
 const makeQuery = function (query) {
   return new Promise(function (resolve) {
     let socket = new WebSocket("ws://localhost:3000/");
@@ -12,25 +16,44 @@ const makeQuery = function (query) {
   });
 }
 
-const protocol = function (src, dst) {
-  const query = hash([src, dst], TOTAL_COUNT);
-  return makeQuery(query).then(function (response) {
-    // Find out what the response mean (search for hash).
-    // I.e. transform response to the next hop point id!
-    const srcPoint = getPointById(src);
-    const neighbors = srcPoint.properties["neighbors"].slice();
-    neighbors.push(0);  // Special case: unreachable destinations.
+// Main entry point to our protocol: makes repeated queries until destination
+// is reached.
+window.protocol = async function (src, dst) {
+  const query = hash([src, dst], UI.TOTAL_COUNT);
+  const response = await makeQuery(query);
 
-    let next = null;
-    for (let i = 0; i < neighbors.length; i++) {
-      const candidateResponse = [src, dst, neighbors[i]];
-      if (response == hash(candidateResponse, TOTAL_COUNT)) {
-        return neighbors[i];
-      }
+  // Find out what the response mean (search for hash).
+  // I.e. transform response to the next hop point id!
+  const srcPoint = UI.getPointById(src);
+  const neighbors = srcPoint.properties["neighbors"].slice();
+  neighbors.push(0);  // Special case: unreachable destinations.
+
+  // Hash search.
+  let next = null;
+  for (let i = 0; i < neighbors.length; i++) {
+    const candidateResponse = [src, dst, neighbors[i]];
+    if (response == hash(candidateResponse, UI.TOTAL_COUNT)) {
+      next = neighbors[i];
     }
+  }
 
+  // Unrecognized hash!
+  if (next == null) {
     throw new Error(
         "Internal Error: response returned unrecognizable hash " + response);
-  });
-}
+  }
 
+  // Is the destination unreachable?
+  if (next === 0) {
+    alert("Destination unreachable!");
+    return;
+  }
+
+  // Draw path!
+  UI.drawPath([src, next]);
+  if (next != dst) {  // Keep going.
+    protocol(next, dst);
+  }
+};
+
+})();
--- client/ui.js
+++ client/ui.js
@@ -1,45 +1,25 @@
+/* global L */
+(function (exports) {
+
 // Code that is responsible for displaying the nodes on the map and drawing
 // and styling map things, including clicks and paths.
-
 $( document ).ready(function() {
   document.getElementById('leaflet').style.cursor = 'crosshair';
 });
 
-const TOTAL_COUNT = points.features.length;
+// Private state.
 let pointCount = 0;
 var path = Array();
 var isPathRendered = false;
 
-function getPointById(id) {
-  for(var i = 0; i < points.features.length; i++) {
-    if(points.features[i].properties.point_id == id)
-      return points.features[i];
-  }
-  return null;
-}
-
-function randomColor() {
+// Display the locations on the map.
+const randomColor = function () {
   var color = '', letters = '0123456'; //789ABCDEF
   for (var i=0; i<6; i++) {color += letters[Math.floor(Math.random() * letters.length)];}
   return '#' + color;
-}
+};
 
-function drawLineBetweenPoints(start, stop) {
-  //start and stop are strng arrays containng start and stop latlns respectively
-  var latlngs = Array();
-  latlngs.push(start);
-  latlngs.push(stop);
-  // latlngs.push([parseFloat(start.split(",")[0]), parseFloat(start.split(",")[1])])
-  // latlngs.push([parseFloat(stop.split(",")[0]), parseFloat(stop.split(",")[1])])
-  var polyline = L.polyline(latlngs, {color: 'midnightblue'});
-  path.push(polyline);
-  polyline.addTo(leaflet);
-  //Optional, enable for zooming in on the map area with line
-  //leaflet.fitBounds(polyline.getBounds());
-
-}
-
-var leaflet = L.map('leaflet').setView([42.3469,-71.0709], 17);
+const leaflet = L.map('leaflet').setView([42.3469,-71.0709], 17);
 L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom:20, attribution:'', id:'mapbox.light'
 }).addTo(leaflet);
@@ -76,12 +56,48 @@ L.geoJson(points, {
     }
 }).addTo(leaflet);
 
-function drawPath(points) {
+const pointsSelected = async function () {
+  const src = window.localStorage.getItem('StartPointId');
+  const dst = window.localStorage.getItem('StopPointId');
+
+  try {
+    await window.protocol(src, dst);
+  } catch (error) {
+    alert("Internal error!");
+    console.log(error);
+  }
+};
+
+// Internal helpers for drawing paths / lines.
+const drawLineBetweenPoints = function (start, stop) {
+  //start and stop are strng arrays containng start and stop latlns respectively
+  var latlngs = Array();
+  latlngs.push(start);
+  latlngs.push(stop);
+  // latlngs.push([parseFloat(start.split(",")[0]), parseFloat(start.split(",")[1])])
+  // latlngs.push([parseFloat(stop.split(",")[0]), parseFloat(stop.split(",")[1])])
+  var polyline = L.polyline(latlngs, {color: 'midnightblue'});
+  path.push(polyline);
+  polyline.addTo(leaflet);
+  //Optional, enable for zooming in on the map area with line
+  //leaflet.fitBounds(polyline.getBounds());
+};
+
+// UI API.
+exports.TOTAL_COUNT = points.features.length;
+exports.getPointById = function (id) {
+  for(var i = 0; i < points.features.length; i++) {
+    if(points.features[i].properties.point_id == id)
+      return points.features[i];
+  }
+  return null;
+};
+exports.drawPath = function (points) {
   pointCount = 0;
   for (var jump = 0; jump < points.length; jump++) {
     if (jump < points.length-1) {
-      source = getPointById(points[jump])["coordinates"].slice().reverse();
-      dest = getPointById(points[jump+1])["coordinates"].slice().reverse();
+      source = exports.getPointById(points[jump])["coordinates"].slice().reverse();
+      dest = exports.getPointById(points[jump+1])["coordinates"].slice().reverse();
       drawLineBetweenPoints(source,dest)
     }
 
@@ -103,4 +119,6 @@ function drawPath(points) {
       }
     });
   }
-}
+};
+
+})(window.UI = {});
