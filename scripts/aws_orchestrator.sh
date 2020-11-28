33dc6605af1da47ef158756afcdde662d6920f9a
Kinan Dak Albab <kinan.bab.14@gmail.com>
2020-11-27 20:21:28 -0500

    Add orchestrator AWS installation script

---
+++ scripts/aws_orchestrator.sh
@@ -0,0 +1,48 @@
+Content-Type: multipart/mixed; boundary="//"
+MIME-Version: 1.0
+
+--//
+Content-Type: text/cloud-config; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="cloud-config.txt"
+
+#cloud-config
+cloud_final_modules:
+- [scripts-user, always]
+
+--//
+Content-Type: text/x-shellscript; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="userdata.txt"
+
+#!/bin/bash
+
+# clone repo once!
+if [[ ! -d "drivacy" ]]
+then
+  git clone https://github.com/multiparty/drivacy.git
+fi
+
+# update repo.
+cd drivacy
+git checkout c++
+git pull origin c++
+git submodule init
+git submodule update
+
+# Bazel
+sudo apt install curl gnupg
+curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > bazel.gpg
+sudo mv bazel.gpg /etc/apt/trusted.gpg.d/
+echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+sudo apt-get update && sudo apt-get install -y bazel
+
+# Dependencies
+sudo apt-get install -y g++
+sudo apt-get install -y build-essential
+
+# Compile with bazel
+bazel build drivacy:config
+--//
