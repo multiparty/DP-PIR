8006de06a1bf229bfb002cd36cafe18620533bf8
PetitPenguin <ra1issa@bu.edu>
2020-12-03 18:26:32 -0500

    Make keys unique, Hardcode max value to 10^8

--- scripts/gen_table.py
+++ scripts/gen_table.py
@@ -8,19 +8,15 @@ import sys
 # value_size = sys.argv[3]
 
 table_size = input("Enter table size: ")
-key_size = input("Enter key size in bits: ")
-value_size = input("Enter value size in bits: ")
 
-max_key = pow(2,int(key_size))
-max_value = pow(2,int(value_size))
+max_value = pow(10,8)
 
-print("Max key:", max_key)
 print("Max value:", max_value)
 
 table = []
 for i in range(int(table_size)):
     pair = {}
-    pair["key"] = randint(0, max_key)
+    pair["key"] = i
     pair["value"] = randint(0, max_value)
     table.append(pair)
 
